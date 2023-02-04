50930d221ce5d16e6df5fd376c19dea17a805e51
cmc17300 <123524441+cmc17300@users.noreply.github.com>
2023-02-04 16:32:06 -0600

    Create ElementTypes.py

---
+++ ElementTypes.py
@@ -0,0 +1,8 @@
+# append different types of elements to a list and check the types
+# question 3
+x = []
+a = [23,'python',23.98]
+x.append(type(a[0]))
+x.append(type(a[1]))
+x.append(type(a[2]))
+print(x)
