a1bbceae96d78a4cf6c9f95b8baf64db8b823fd0
cmc17300 <123524441+cmc17300@users.noreply.github.com>
2023-02-04 16:22:22 -0600

    Create StarPattern.py

---
+++ StarPattern.py
@@ -0,0 +1,11 @@
+# star pattern in python
+n = 5
+for i in range(n):
+    for j in range(i + 1):
+        print('*', end="")
+    print()
+
+for i in range(n):
+    for j in range(n - i - 1):
+        print('*', end="")
+    print()
